42e17c42eeefa984cf5dfb42d5269f4615ed0624
Vany1012 <alvarezvillalvavanya@gmail.com>
2026-01-13 11:40:00 -0600

    Subiendo el modelo de tickets y vendedores

--- models/ticketModels.js
+++ models/ticketModels.js
@@ -1,1 +1,8 @@
-hola
+const mongoose = require('mongoose');
+const ticketSchema = new mongoose.Schema({
+    numTicket: { type:String, required: true, trim: true },
+    fechaHora: { type: Date, required: true, default: Date.now},
+    productos: { type: [String], default: []},
+    total: { type:String, require: true , trim: true},
+});
+module.exports = mongoose.model('ticket', ticketSchema);
