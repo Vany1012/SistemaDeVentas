042ef15694dec963c32f3cabf49795769ab0b3ac
Vany1012 <alvarezvillalvavanya@gmail.com>
2026-01-14 09:51:57 -0600

    Subiendo el server

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-hola
+// Importamos las dependencias
+const express = require ('express');
+const dotenv = require ('dotenv');
+const cors = require ('cors');
+const connectDB = require ('./config/db');
+const vendedorRoutes = require ('./routes/vendedorRoute');
+const inventarioRoutes = require ('./routes/inventarioRoute');
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/vendedor', vendedorRoutes);
+app.use('/api/inventario', inventarioRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {console.log(`Servidor lenvantado en el puerto: ${PORT}`)});
