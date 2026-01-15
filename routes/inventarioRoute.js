d36c4aa37851d998bbe4ade895b6ccda4f2161fb
El-Ayvar <=>
2026-01-15 09:21:39 -0600

    Realizando ruta de inventario

--- routes/inventarioRoute.js
+++ routes/inventarioRoute.js
@@ -1,1 +1,10 @@
-holahola
+const express = require('express');
+const inventarioRouter = express.Router();
+const inventarioController = require('../controllers/inventarioController');
+const { protect } = require('../middleware/authMiddleware');
+
+inventarioRouter.post('/createProducto', protect, inventarioController.createProducto);
+inventarioRouter.delete('/deleteProducto', protect, inventarioController.deleteProducto);
+inventarioRouter.get('/verInventario', protect, inventarioController.verInventario);
+
+module.exports = inventarioRouter;
