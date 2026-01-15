429051ebce6007ca62addba2bd669702ad634b4f
Josue <21387996@uagro.mx>
2026-01-15 08:55:49 -0600

    subiendo rutas login y register

--- routes/vendedorRoute.js
+++ routes/vendedorRoute.js
@@ -1,1 +1,13 @@
-hola
+//instanciamos express
+const express = require('express');
+const {registerVendedor, loginVendedor} = require('../controllers/vendedorController');
+const vendedorRouter = express.Router();
+
+const {protect, authorizeRoles} = require('../middleware/vendedorMiddleware');
+
+//instanciamos el router y declaramos dos rutas, login y register.
+vendedorRouter.post('/register', registerVendedor);
+vendedorRouter.post('/login', loginVendedor);
+
+//exportamos router
+module.exports = vendedorRouter;
