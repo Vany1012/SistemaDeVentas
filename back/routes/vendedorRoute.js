830ee0f5f7806a5e3485a6fd727a57ae0e6c4bbd
Axelinpinguin <axel.0302@outlook.es>
2026-01-21 22:13:09 -0600

    Agregando el protect a vendedorRoutes.js

--- back/routes/vendedorRoute.js
+++ back/routes/vendedorRoute.js
@@ -5,7 +5,7 @@ const vendedorRouter = express.Router();
 const {protect} = require('../middleware/vendedorMiddleware');
 
 vendedorRouter.route('/register') // Ruta para registrar un nuevo vendedor solo admin, te pide en el body vendedorName, email, password, role, vendedorId, active este es opcional, por defecto es true
-.post( registerVendedor);
+.post(protect, registerVendedor);
 
 vendedorRouter.post('/login', loginVendedor); // Ruta para login de vendedor, te pide en el body email y password
 
